79b423516500147c77c1dd65bcc456f60d9f2138
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2019-12-31 15:37:20 +0000

    Use data from GitLab in the table

--- src/components/GitLabEnvironments.vue
+++ src/components/GitLabEnvironments.vue
@@ -14,7 +14,7 @@
             </v-card-title>
             <v-data-table
                     :headers="headers"
-                    :items="desserts"
+                    :items=environments()
                     :search="search"
             />
         </v-card>
@@ -27,101 +27,31 @@
     data() {
       return {
         search  : '',
-        headers : [
+        headers: [
           {
-            text    : 'Dessert (100g serving)',
-            align   : 'left',
+            text: 'Name',
+            align:'left',
             sortable: false,
-            value   : 'name',
+            value: 'name'
           },
-          {text: 'Calories', value: 'calories'},
-          {text: 'Fat (g)', value: 'fat'},
-          {text: 'Carbs (g)', value: 'carbs'},
-          {text: 'Protein (g)', value: 'protein'},
-          {text: 'Iron (%)', value: 'iron'},
+          {text: 'Branch', value: 'branch'},
+          {text: 'Status', value: 'status'},
+          {text: 'Deployed by', value: 'by'},
+          {text: 'Deployed on', value: 'on'},
         ],
-        desserts: [
-          {
-            name    : 'Frozen Yogurt',
-            calories: 159,
-            fat     : 6.0,
-            carbs   : 24,
-            protein : 4.0,
-            iron    : '1%',
-          },
-          {
-            name    : 'Ice cream sandwich',
-            calories: 237,
-            fat     : 9.0,
-            carbs   : 37,
-            protein : 4.3,
-            iron    : '1%',
-          },
-          {
-            name    : 'Eclair',
-            calories: 262,
-            fat     : 16.0,
-            carbs   : 23,
-            protein : 6.0,
-            iron    : '7%',
-          },
-          {
-            name    : 'Cupcake',
-            calories: 305,
-            fat     : 3.7,
-            carbs   : 67,
-            protein : 4.3,
-            iron    : '8%',
-          },
-          {
-            name    : 'Gingerbread',
-            calories: 356,
-            fat     : 16.0,
-            carbs   : 49,
-            protein : 3.9,
-            iron    : '16%',
-          },
-          {
-            name    : 'Jelly bean',
-            calories: 375,
-            fat     : 0.0,
-            carbs   : 94,
-            protein : 0.0,
-            iron    : '0%',
-          },
-          {
-            name    : 'Lollipop',
-            calories: 392,
-            fat     : 0.2,
-            carbs   : 98,
-            protein : 0,
-            iron    : '2%',
-          },
-          {
-            name    : 'Honeycomb',
-            calories: 408,
-            fat     : 3.2,
-            carbs   : 87,
-            protein : 6.5,
-            iron    : '45%',
-          },
-          {
-            name    : 'Donut',
-            calories: 452,
-            fat     : 25.0,
-            carbs   : 51,
-            protein : 4.9,
-            iron    : '22%',
-          },
-          {
-            name    : 'KitKat',
-            calories: 518,
-            fat     : 26.0,
-            carbs   : 65,
-            protein : 7,
-            iron    : '6%',
-          },
-        ],
+        environments() {
+          return this.$store.state.environments.map(function (environment) {
+            let lastDeployment = environment.last_deployment
+
+            return {
+              name: environment.name,
+              branch: lastDeployment ? lastDeployment.ref : 'NA',
+              status: lastDeployment ? lastDeployment.deployable.status : 'NA',
+              by: lastDeployment ? lastDeployment.user.name : 'NA',
+              on: lastDeployment ? lastDeployment.deployable.finished_at : 'NA',
+            }
+          })
+        },
       }
     },
     created() {
