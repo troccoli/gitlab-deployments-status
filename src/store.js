d9470c67e89f00020d07216f387740e385e4db44
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2019-12-31 10:57:00 +0000

    Add a Vuex store

---
+++ src/store.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import GitLabService from "./services/GitLabService";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state    : {
+    environments: [],
+  },
+  mutations: {
+    SET_ENVIRONMENTS(state, environments) {
+      state.environments = environments;
+    },
+  },
+  actions  : {
+    fetchEnvironments({commit}) {
+      return GitLabService.getEnvironments()
+        .then(response => {
+          commit("SET_ENVIRONMENTS", response.data.data);
+          return this;
+        })
+        .catch(error => {
+          console.log('There was an error: ', error.response);
+        });
+    },
+  }
+})
